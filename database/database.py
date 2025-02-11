e6bec60a10a7e53eae285f09c48c2908a8cf2828
vhzkclq0705 <vhzkclq0705@naver.com>
2025-02-11 15:40:30 +0900

    View를 통한 쿼리 구문 최적화

--- database/database.py
+++ database/database.py
@@ -26,48 +26,7 @@ class Database:
             row = cur.fetchall()
             return row
 
-    # def insert_data(self, lunch_menu: LunchMenu):
-    #     members = self.get_member_dict()
-    #     data = (lunch_menu.menu_name, members[lunch_menu.member_name], lunch_menu.date)
-    #     query = '''
-    #     INSERT INTO lunch_menu (menu_name, member_id, dt)
-    #     VALUES (%s, %s, %s)
-    #     '''
-    #     self.execute_query(query, data)
-
-    # def get_member_need_enter(self) -> list:
-    #     query = '''
-    #     select
-    #         m.name
-    #     from
-    #         member m
-    #     left join lunch_menu l
-    #         on m.id = l.member_id and l.dt = current_date
-    #     where
-    #         l.member_id is null
-    #     '''
-    #     self.execute_query(query)
-    #     results = self.cursor.fetchall()
-
-    #     return ',  '.join([record[0] for record in results])
-
     def select_data(self) -> list:
-        query = '''
-        SELECT
-            l.menu_name,
-            m.name,
-            l.dt
-        FROM lunch_menu l
-        JOIN member m ON l.member_id = m.id
-        '''
-        
+        query = "SELECT * FROM view_select_all"
         data = self.execute_query(query)
         return data
-
-    # def get_member_dict(self) -> dict:
-    #     query = '''
-    #     SELECT jsonb_object_agg(name, id)
-    #     FROM member;
-    #     '''
-    #     self.execute_query(query)
-    #     return self.cursor.fetchone()[0]

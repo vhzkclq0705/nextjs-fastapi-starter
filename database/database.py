a15aec281935551958f803dbab5e03c0fe3c1c00
vhzkclq0705 <vhzkclq0705@naver.com>
2025-02-11 16:05:05 +0900

    웹 DB와 연동

--- database/database.py
+++ database/database.py
@@ -5,11 +5,11 @@ from psycopg.rows import dict_row
 
 load_dotenv()
 db_config = {
-    "dbname": os.getenv('DB_NAME'),
-    "user": os.getenv('DB_USERNAME'),
-    "password": os.getenv('DB_PASSWORD'),
-    "host": os.getenv('DB_HOST'),
-    "port": os.getenv('DB_PORT')
+    "dbname": os.getenv("POSTGRES_DATABASE"),
+    "user": os.getenv("POSTGRES_USER"),
+    "password": os.getenv("POSTGRES_PASSWORD"),
+    "host": os.getenv("POSTGRES_HOST"),
+    "port": os.getenv("POSTGRES_PORT", "5432")
 }
 
 # Database management class
